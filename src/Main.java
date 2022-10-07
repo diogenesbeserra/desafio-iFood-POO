c12a9987814c22f911fc431fce57108e7f6421ee
Diógenes Beserra <diogenesbeserra@gmail.com>
2022-10-07 16:50:48 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
 
-        System.out.println("Hello world!");
+        curso1.setDescricao("descrição curso java");
+        curso1.setTitulo("Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setDescricao("descrição curso javaScript");
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setCargaHoraria(12);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
